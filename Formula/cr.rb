45a5fa1fcabd12398e88f5a1f4718e12966ce412
Kirill Sapchuk <kirill@commonroom.io>
2026-05-28 11:08:35 -0700

    Release 0.1.1

--- Formula/cr.rb
+++ Formula/cr.rb
@@ -1,8 +1,8 @@
 class Cr < Formula
-  desc "Common Room from the command line"
+  desc "Common Room from the command-line"
   homepage "https://www.commonroom.io"
-  url "https://registry.npmjs.org/@commonroomio/cli/-/cli-0.1.0.tgz"
-  sha256 "75e2158bba494e7c7b8c3b977277b2fcef3341df9fa55232ad51162a9ac7db6b"
+  url "https://registry.npmjs.org/@commonroomio/cli/-/cli-0.1.1.tgz"
+  sha256 "a617f94a61314063b914fa0252a9cf7f73dfd89192eacc083d4c8fdc81b59a91"
   license "Apache-2.0"
 
   depends_on "node"
